cb9d87e3cb41bb62cf25dd9170fa94d03be3fab0
mario.arista <mario.arista.dev@gmail.com>
2024-05-14 17:17:41 +0200

    Migration & Seeder of Categories + Restaurant & Category Model

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Cinese',
+            'Pizzeria',
+            'Messicano',
+            'Giapponese',
+            'Fast food',
+            'Vegetariano',
+            'Vegano',
+            'Italiano',
+            'Hamburger',
+            'Sushi',
+            'Kebab',
+            'Gourmet',
+        ];
+
+        foreach($categories as $category) {
+
+            $newCategory = new Category();
+
+            $newCategory->category_name = $category;
+
+            $newCategory->save();
+
+        }
+    }
+}
