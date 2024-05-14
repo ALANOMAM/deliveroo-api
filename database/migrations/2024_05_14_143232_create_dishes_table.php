0592bb7bdf23fa3bd0b11c19c37f6968671a3c32
alan_omam <omamalan@hotmail.it>
2024-05-14 18:20:38 +0200

    impostato migrazione e seeder piatti

--- database/migrations/2024_05_14_143232_create_dishes_table.php
+++ database/migrations/2024_05_14_143232_create_dishes_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('dishes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('restaurant_id')->constrained();
+            $table->string('dish_name',50);
+            $table->decimal('dish_price',5,2);
+            $table->boolean('visible');
+            $table->text('dish_image')->nullable();
+            $table->text('ingredients')->nullable();
             $table->timestamps();
         });
     }
