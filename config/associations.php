b3255b80824cd64a11d7e96b8e65903fec19765b
mario.arista <mario.arista.dev@gmail.com>
2024-05-21 19:40:24 +0200

    seeder funzionante

--- config/associations.php
+++ config/associations.php
@@ -9,7 +9,7 @@ return [
     ['dish_id' => 17, 'order_id' => 2, 'price' => 17, 'quantity' => 1,],
     ['dish_id' => 18, 'order_id' => 2, 'price' => 25, 'quantity' => 4,],
 
-    ['dish_id' => 5, 'order_id' => 3, 'price' => 9, 'quantity' => 1,],
+    ['dish_id' => 6, 'order_id' => 3, 'price' => 9, 'quantity' => 1,],
     ['dish_id' => 7, 'order_id' => 3, 'price' => 12, 'quantity' => 1,],
     ['dish_id' => 8, 'order_id' => 3, 'price' => 16, 'quantity' => 2,],
 
