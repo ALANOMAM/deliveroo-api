d9dcdbba623332382e64e3aa835babbb6359c43f
MiriamEvardi <miriamevardi97@gmail.com>
2024-05-26 19:13:40 +0200

    dashboard page finished ( with responsive)

--- resources/views/admin/restaurants/dashboard.blade.php
+++ resources/views/admin/restaurants/dashboard.blade.php
@@ -1,56 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <div class="d-flex flex-column pb-2">
-        <div class="jumbo">
-            <div class="welcome-message">
-                <h2 class="fs-3">Bentornato/a, {{ Auth::user()->name === null ? 'Ristoratore' : Auth::user()->name }}!</h2>
-                <h1>{{ $restaurant->restaurant_name }} è su JustBool!</h1>
+<div class="container p-0 position-relative overflow-hidden">
+    <div class="jumbo mt-4">
+        <div class="welcome-message">
+            <div class="fs-5">Ciao, {{ Auth::user()->name ?? 'Ristoratore' }},</div>
+            <h1 class="text-white  display-4 fw-normal">{{ $restaurant->restaurant_name }} è su JustBool!</h1>
+            <div class="list-group-item d-flex flex-wrap mt-4 gap-2 pb-5">
+                @foreach ($restaurant->categories as $category)
+                <span class="badge rounded-pill bg-white text-dark fs-6 fw-normal">{{ $category->category_name }}</span>
+                @endforeach
             </div>
-            @if ($restaurant->image)
-            @if (Str::startsWith($restaurant->image, ['http://', 'https://']))
-            <img src="{{ $restaurant->image }}" alt="{{ $restaurant->restaurant_name }}">
-            @else
-            <img src="{{ asset('storage/' . $restaurant->image) }}" alt="{{ $restaurant->restaurant_name }}">
-            @endif
-            @else
-            <img src="{{ Vite::asset('resources/img/restaurant_placeholder.jpg') }}" alt="Placeholder">
-            @endif
         </div>
+
+        <a href="{{ route('admin.dishes.index') }}" class="btn btn-primary d-md-block d-none menu-button position-absolute p-2 px-3" style="bottom: 60px; left: 60px;">Vai al tuo menu</a>
+
+        @if ($restaurant->image)
+        @if (Str::startsWith($restaurant->image, ['http://', 'https://']))
+        <img src="{{ $restaurant->image }}" alt="{{ $restaurant->restaurant_name }}">
+        @else
+        <img src="{{ asset('storage/' . $restaurant->image) }}" alt="{{ $restaurant->restaurant_name }}">
+        @endif
+        @else
+        <img src="{{ Vite::asset('resources/img/restaurant_placeholder.jpg') }}" alt="Placeholder">
+        @endif
     </div>
 
-    <!-- CATEGORIE RISTORANTE -->
-    <div>
-        <div class="list-group-item d-flex justify-content-center mt-4 gap-2">
-            @foreach ($restaurant->categories as $category)
-            <span class="badge rounded-pill text-bg-success fs-5">{{ $category->category_name }}</span>
-            @endforeach
-        </div>
+    <div class="info-section mt-5 ms-5">
+        <div class="row">
 
-        <div class="col-md-12 my-5 d-flex justify-content-between m-auto gap-5">
-            <div class="fs-5 d-flex flex-column">
-                <div class="pb-1">
-                    <strong>Nome Attività: </strong> {{ $restaurant->restaurant_name }}
+            <div class="col-md-12">
+                <h3 class="fw-semibold text-dark">{{ $restaurant->restaurant_name }}</h3>
+            </div>
+
+            <div class="col-md-6">
+                <div class="pb-4 fs-5">
+                    {{ $restaurant->address }}
                 </div>
                 <div class="pb-1">
-                    @if ($restaurant->address)
-                    <strong>Indirizzo:</strong> {{ $restaurant->address }}
-                    @endif
+                    {{ $restaurant->phone }}
                 </div>
-                <div class="pb-1">
-                    @if ($restaurant->phone)
-                    <strong>Telefono:</strong> {{ $restaurant->phone }}
-                    @endif
-                </div>
-                <div class="pb-1">
-                    @if ($restaurant->description)
-                    <strong>Descrizione:</strong> {{ $restaurant->description }}
-                    @endif
+                <div class="pb-4">
+                    P. IVA {{ $restaurant->vat}}
                 </div>
             </div>
-            <div class="fs-5">
-                <strong>P. IVA:</strong> {{ $restaurant->vat }}
+
+
+            <div class="col-md-6">
+                <div class="pb-1">
+                    <p class="text-wrap me-5 " style="word-wrap: break-word;">{{ $restaurant->description }}</p>
+                </div>
             </div>
         </div>
     </div>
