63aa258239e7b417a25a0567104e742de492a3c8
mario.arista <mario.arista.dev@gmail.com>
2024-05-27 12:30:18 +0200

    correction OrderConfirmation

--- app/Mail/OrderConfirmationMail.php
+++ app/Mail/OrderConfirmationMail.php
@@ -31,7 +31,7 @@ class OrderConfirmationMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Confirma di ordine effetuato',
+            subject: 'Conferma di ordine effetuato',
         );
     }
 
@@ -41,7 +41,7 @@ class OrderConfirmationMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.order_confirmation',
         );
     }
 
