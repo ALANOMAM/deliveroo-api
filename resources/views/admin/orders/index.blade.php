02bed3c1ce841ac52fd3f6b323032ca293aecf95
mario.arista <mario.arista.dev@gmail.com>
2024-05-28 01:38:30 +0200

    tolto riepilogo ordine

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -11,12 +11,13 @@
             {{-- Intestazione Tabella --}}
             <thead class="bg-transparent">
                 <tr>
-                    <th scope="col">Cliente</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Cognome</th>
                     <th scope="col">Indirizzo consegna</th>
                     <th scope="col">Recapiti</th>
                     <th scope="col" class="text-center">Data Ordine</th>
-                    <th scope="col" class="text-center">Riepilogo</th>
                     <th scope="col" class="text-center">Totale ordine</th>
+                    {{-- <th scope="col" class="text-center">Riepilogo</th> --}}
 
                 </tr>
             </thead> 
@@ -33,7 +34,13 @@
 
                     <th class="align-middle" scope="row">
                         <div class="fw-bold">
-                            {{ $order->customer_name }} {{ $order->customer_surname }}
+                            {{ $order->customer_name }}
+                        </div>
+                    </th>
+
+                    <th class="align-middle" scope="row">
+                        <div class="fw-bold">
+                            {{ $order->customer_surname }}
                         </div>
                     </th>
 
@@ -62,7 +69,7 @@
 
                     {{-- data ordine --}}
 
-                    <th class="orders-data align-middle" scope="row">
+                    <th scope="row">
                         <div class="fw-normal d-flex justify-content-center align-items-center">
                             <?php
                                 setlocale(LC_TIME, 'it_IT.UTF-8'); // Imposta la lingua italiana
@@ -74,27 +81,25 @@
                             <span class="order-date bg-success text-white px-2 py-1 rounded mb-1"><?php echo $formatted_date . ' ' . $formatted_time; ?></span>
                         </div>
                     </th>
-
-                    {{-- Riepiologo ordine --}}
-                
-                    <th class="orders-price align-middle text-center" scope="row">
-                        
-                        {{-- <i id="eye-icon" class="fa-solid fa-eye toggle-details"></i> --}}
-                        
-                        @foreach($order->dishes as $dish)
-                        <div class="fw-normal">
-                          <small id="order-details">{{ $dish->pivot->quantity }} x {{ $dish->dish_name }} = {{ $dish->pivot->price }}€</small>
-                        </div>
-                        @endforeach
-                    </th>
-
+                    
                     {{-- Prezzo totale ordine --}}
 
-                    <th class="orders-price align-middle text-center" scope="row">
+                    <th class="orders-price text-center" scope="row">
                         <span class="fw-normal fs-5">
                             {{ $order->total_price }} €
                         </span>
                     </th>
+
+                    {{-- <th class="orders-price align-middle text-center" scope="row">
+                        
+                        <i id="eye-icon" class="fa-solid fa-eye toggle-details"></i>
+                        
+                        @foreach($order->dishes as $dish)
+                        <div class="fw-normal d-none">
+                          <span id="order-details">{{ $dish->pivot->quantity }} x {{ $dish->dish_name }} = {{ $dish->pivot->price }}€</span>
+                        </div>
+                        @endforeach
+                    </th> --}}
 
                 </tr>
                 @endforeach
