b43f188854db912887f9db6380028ce7c70bf9bc
mario.arista <mario.arista.dev@gmail.com>
2024-05-28 00:37:35 +0200

    stile pagina ordini

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -11,11 +11,14 @@
             {{-- Intestazione Tabella --}}
             <thead class="bg-transparent">
                 <tr>
-                    <th scope="col">Utente</th>
-                    <th scope="col">Recapiti</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Cognome</th>
                     <th scope="col">Indirizzo consegna</th>
-                    <th scope="col">Totale ordine</th>
-                    <th class="text-center" scope="col">Data Ordine</th>
+                    <th scope="col">Recapiti</th>
+                    <th scope="col" class="text-center">Data Ordine</th>
+                    <th scope="col" class="text-center">Totale ordine</th>
+                    
+
                 </tr>
             </thead> 
             
@@ -27,46 +30,68 @@
                 @foreach ($orders as $order)
 
 
-                <tr class="shadow">
+                <tr class="orders-shadow">
 
                     {{-- Nome Cognome utente --}}
 
-                    <th class="title-dish-row" scope="row">
+                    <th class="align-middle" scope="row">
                         <div class="fw-bold">
-                            {{ $order->customer_name }} {{ $order->customer_surname }}
+                            {{ $order->customer_name }}
                         </div>
                     </th>
 
-                    {{-- Recapiti utente --}}
-
-                    <th class="title-dish-row" scope="row">
-                        <div class="fw-normal">
-                            {{ $order->customer_email }} - {{ $order->customer_phone }}
+                    <th class="align-middle" scope="row">
+                        <div class="fw-bold">
+                            {{ $order->customer_surname }}
                         </div>
                     </th>
+
 
                     {{-- Indirizzo consegna --}}
-                    <th class="ingedients-dish-row" scope="row">
+                    <th class="align-middle" scope="row">
                         <div class="fw-normal">
                             {{ $order->customer_address }}
                         </div>
                     </th>
 
-                    {{-- Prezzo totale ordine --}}
+                    {{-- Recapiti utente --}}
 
-                    <th class="ingedients-dish-row" scope="row">
+                    <th class="orders-data align-middle" scope="row">
+                    
                         <div class="fw-normal">
-                            {{ $order->total_price }} €
+                            <i class="fa-solid fa-envelope"></i>
+                            {{ $order->customer_email }}
                         </div>
+                        <div class="fw-normal">
+                            <i class="fa-solid fa-phone"></i>
+                            {{ $order->customer_phone }}
+                        </div>
+                        
                     </th>
 
                     {{-- data ordine --}}
 
-                    <th class="ingedients-dish-row" scope="row">
-                        <div class="fw-normal">
-                            {{ $order->created_at }}
+                    <th scope="row">
+                        <div class="fw-normal d-flex justify-content-center align-items-center">
+                            <?php
+                                setlocale(LC_TIME, 'it_IT.UTF-8'); // Imposta la lingua italiana
+                                $created_at = $order->created_at;
+                                $timestamp = strtotime($created_at);
+                                $formatted_date = strftime('%d %b %Y', $timestamp);
+                                $formatted_time = date('H:i', $timestamp);
+                            ?>
+                            <span class="order-date bg-success text-white px-2 py-1 rounded mb-1"><?php echo $formatted_date . ' ' . $formatted_time; ?></span>
                         </div>
                     </th>
+                    
+                    {{-- Prezzo totale ordine --}}
+
+                    <th class="orders-price text-center" scope="row">
+                        <span class="fw-normal fs-5 ">
+                            {{ $order->total_price }} €
+                        </span>
+                    </th>
+
 
 
                 </tr>
